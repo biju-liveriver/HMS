d9ef00081b73aab782c0ad9010e45548e0a722d6
samirpalai <samir.liveriver@gmail.com>
2014-01-15 00:25:15 +0530

    Update login.js

--- hms-web/src/main/webapp/styles/login.js
+++ hms-web/src/main/webapp/styles/login.js
@@ -1,11 +1,8 @@
 function HMSCtrl_login($scope, $http) {  
   $scope.login = function() {
     var inputData = {userName:$scope.useNameText, password:$scope.passwordText};
-    alert("User Name::"+inputData.userName +"  Password:"+inputData.password);
-   
-    $http.get('hms-web/rest/user/login',inputData).success(function(data) {
+    $http.post('/hms/rest/user/login', inputData).success(function(data) {
 		$scope.serverResponse = data;
-		 alert("Server Reponse"+data);
 	  });
     
   };
